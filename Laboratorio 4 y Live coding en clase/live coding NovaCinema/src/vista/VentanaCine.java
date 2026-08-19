7750bdddca96e5f6d8c83c397f8036b095f779af
kendallDVM <kendall.valencia.murillo@est.una.ac.cr>
2026-08-19 11:17:08 -0600

    intento de interfaz 😓🥲
    se intento🥲

---
+++ Laboratorio 4 y Live coding en clase/live coding NovaCinema/src/vista/VentanaCine.java
@@ -0,0 +1,19 @@
+package vista;
+
+import javax.swing.*;
+
+public class VentanaCine {
+    private JPanel panelRaiz;
+    private JPanel panelEncabezado;
+    private JLabel lblTitulo;
+    private JTabbedPane tabPrincipal;
+    private JPanel tabCartelera;
+    private JPanel pnlFormPelicula;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField7;
+    private JComboBox comboBox1;
+}
